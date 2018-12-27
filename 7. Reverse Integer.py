ce1823543e5db9234470f01e126749bc1feb0d1b
Flash Wang <phynew0111@gmail.com>
2018-12-27 19:23:08 +0800

    Update 7. Reverse Integer.py

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -17,3 +17,21 @@
 # Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: 
 # [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 
+def reverse(x):
+    """
+    :type x: int
+    :rtype: int
+    """
+    x_abs = abs(x)
+    q = x_abs//10
+    r = x%10
+
+    if q != 0 and r != 0:
+        ans = r 
+
+
+
+
+
+    if x not in range(-2**31, 2**31-1):
+        return 0
