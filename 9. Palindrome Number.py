91d6d7b8589b2d574448735674b1ca51b18c8e36
Flash Wang <phynew0111@gmail.com>
2018-12-28 19:47:22 +0800

    Update 9. Palindrome Number.py

--- 9. Palindrome Number.py
+++ 9. Palindrome Number.py
@@ -17,3 +17,40 @@
 
 # Follow up:
 # Could you solve it without converting the integer to a string?
+
+class Solution:
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        def reverse(x):
+            """
+            :type x: int
+            :rtype: int
+            """
+            r = x % 10
+            q = x // 10
+            ans = r
+
+            while q != 0:
+                r = q % 10
+                q //= 10
+                ans = ans * 10 + r
+
+            return ans
+
+        if x < 0:
+            return False
+
+        rev = reverse(x)
+
+        if rev == x:
+            return True
+
+        else:
+            return False
+          
+# Success
+# Details 
+# Runtime: 468 ms, faster than 30.29% of Python3 online submissions for Palindrome Number.   
