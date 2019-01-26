4eac361b05f74187432b610305be914b8d24840c
Flash Wang <phynew0111@gmail.com>
2019-01-26 18:55:39 +0800

    Update 21. Merge Two Sorted Lists.py

--- 21. Merge Two Sorted Lists.py
+++ 21. Merge Two Sorted Lists.py
@@ -4,3 +4,17 @@
 # Example:
 # Input: 1->2->4, 1->3->4
 # Output: 1->1->2->3->4->4
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
