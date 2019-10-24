5937cf7cef4f3f8c0a2a8840a505d7c5db52cb64
Flash Wang <phynew@hotmail.com>
2019-10-23 21:05:28 -0400

    Update 21. Merge Two Sorted Lists.cpp

--- 21. Merge Two Sorted Lists.cpp
+++ 21. Merge Two Sorted Lists.cpp
@@ -1,1 +1,35 @@
+// Easy
+// Merge two sorted linked lists and return it as a new list. 
+// The new list should be made by splicing together the nodes of the first two lists.
+// Example:
+// Input: 1->2->4, 1->3->4
+// Output: 1->1->2->3->4->4
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (l1 == nullptr) {
+            return l2;
+        } else if (l2 == nullptr) {
+            return l1;
+        } else if (l1->val < l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+    } 
+};
+
+// Details
+// Runtime: 8 ms, faster than 77.45% of C++ online submissions for Merge Two Sorted Lists.
+// Memory Usage: 8.9 MB, less than 77.87% of C++ online submissions for Merge Two Sorted Lists.
