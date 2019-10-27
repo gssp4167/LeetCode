5c602af658dcb9a0e591e9fa38b8764f3a5dfcd6
Ruiyue Wang <phynew@hotmail.com>
2019-10-27 19:39:51 -0400

    Update 141. Linked List Cycle.cpp

--- 141. Linked List Cycle.cpp
+++ 141. Linked List Cycle.cpp
@@ -1,1 +1,62 @@
+// Easy
+// Given a linked list, determine if it has a cycle in it.
 
+// To represent a cycle in the given linked list, 
+// we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. 
+// If pos is -1, then there is no cycle in the linked list.
+
+// Example 1:
+
+// Input: head = [3,2,0,-4], pos = 1
+// Output: true
+// Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+// Example 2:
+
+// Input: head = [1,2], pos = 0
+// Output: true
+// Explanation: There is a cycle in the linked list, where tail connects to the first node.
+
+// Example 3:
+
+// Input: head = [1], pos = -1
+// Output: false
+// Explanation: There is no cycle in the linked list.
+
+// Follow up:
+
+// Can you solve it using O(1) (i.e. constant) memory?
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head == NULL || head->next == NULL) {
+            return false;
+        } else {
+            ListNode *slow = head, *fast = head->next;
+            
+            while (slow != fast) {
+                if (fast == NULL || fast->next == NULL) {
+                    return false;
+                } else {
+                    slow = slow->next;
+                    fast = fast->next->next;
+                }
+            }
+            return true;
+        }
+    }
+};
+
+// Success
+// Details 
+// Runtime: 12 ms, faster than 73.71% of C++ online submissions for Linked List Cycle.
+// Memory Usage: 9.8 MB, less than 80.26% of C++ online submissions for Linked List Cycle.
