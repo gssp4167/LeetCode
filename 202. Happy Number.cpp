c940eba153b86777756aeb3446e70323392302a6
Ruiyue Wang <phynew@hotmail.com>
2019-11-15 16:15:39 -0500

    Update 202. Happy Number.cpp

--- 202. Happy Number.cpp
+++ 202. Happy Number.cpp
@@ -1,18 +1,21 @@
-Easy
+// Easy
 
-Write an algorithm to determine if a number is "happy".
+// Write an algorithm to determine if a number is "happy".
 
-A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+// A happy number is a number defined by the following process: 
+// Starting with any positive integer, replace the number by the sum of the squares of its digits, 
+// and repeat the process until the number equals 1 (where it will stay), 
+// or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
 
-Example: 
+// Example: 
 
-Input: 19
-Output: true
-Explanation: 
-12 + 92 = 82
-82 + 22 = 68
-62 + 82 = 100
-12 + 02 + 02 = 1
+// Input: 19
+// Output: true
+// Explanation: 
+// 12 + 92 = 82
+// 82 + 22 = 68
+// 62 + 82 = 100
+// 12 + 02 + 02 = 1
 
 class Solution {
 public:
@@ -46,10 +49,7 @@ public:
     }
 };
 
-
-
-
-Success
-Details 
-Runtime: 4 ms, faster than 67.11% of C++ online submissions for Happy Number.
-Memory Usage: 8.6 MB, less than 57.69% of C++ online submissions for Happy Number.
+// Success
+// Details 
+// Runtime: 4 ms, faster than 67.11% of C++ online submissions for Happy Number.
+// Memory Usage: 8.6 MB, less than 57.69% of C++ online submissions for Happy Number.
