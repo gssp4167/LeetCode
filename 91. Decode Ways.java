623cdf61c3e04283a870c7aad729f05cbe7580b6
Ruiyue Wang <phynew@hotmail.com>
2020-11-10 00:19:08 -0500

    Update 91. Decode Ways.java

--- 91. Decode Ways.java
+++ 91. Decode Ways.java
@@ -61,3 +61,33 @@ class Solution {
 // Details 
 // Runtime: 1 ms, faster than 95.03% of Java online submissions for Decode Ways.
 // Memory Usage: 37.5 MB, less than 11.87% of Java online submissions for Decode Ways.
+
+class Solution {
+    public int numDecodings(String s) {
+        int a = 1, len = s.length();
+        int b = s.charAt(0) == '0' ? 0 : 1, ans = b;
+           
+        for (int i = 2; i <= len; ++i) {
+            ans = 0;
+            
+            if (s.charAt(i - 1) != '0') {
+                ans += b;     
+            }
+            
+            int two_pre = Integer.valueOf(s.substring(i-2, i));
+            
+            if(two_pre >= 10 && two_pre <= 26) {
+                ans += a;
+            }
+            
+            a = b;
+            b = ans;
+        }
+        return ans;
+    }
+}
+
+// Success
+// Details 
+// Runtime: 1 ms, faster than 95.03% of Java online submissions for Decode Ways.
+// Memory Usage: 37.1 MB, less than 11.87% of Java online submissions for Decode Ways.
