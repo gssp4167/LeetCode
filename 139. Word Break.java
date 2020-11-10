154867bfefdf36972451d34d61cbf2d1e0d3f34f
Ruiyue Wang <phynew@hotmail.com>
2020-11-10 16:37:07 -0500

    Update 139. Word Break.java

--- 139. Word Break.java
+++ 139. Word Break.java
@@ -1,26 +1,26 @@
-Medium
+// Medium
 
-Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, 
-determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+// Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, 
+// determine if s can be segmented into a space-separated sequence of one or more dictionary words.
 
-Note:
-The same word in the dictionary may be reused multiple times in the segmentation.
-You may assume the dictionary does not contain duplicate words.
+// Note:
+// The same word in the dictionary may be reused multiple times in the segmentation.
+// You may assume the dictionary does not contain duplicate words.
 
-Example 1:
-Input: s = "leetcode", wordDict = ["leet", "code"]
-Output: true
-Explanation: Return true because "leetcode" can be segmented as "leet code".
+// Example 1:
+// Input: s = "leetcode", wordDict = ["leet", "code"]
+// Output: true
+// Explanation: Return true because "leetcode" can be segmented as "leet code".
 
-Example 2:
-Input: s = "applepenapple", wordDict = ["apple", "pen"]
-Output: true
-Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
-             Note that you are allowed to reuse a dictionary word.
+// Example 2:
+// Input: s = "applepenapple", wordDict = ["apple", "pen"]
+// Output: true
+// Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+//              Note that you are allowed to reuse a dictionary word.
 
-Example 3:
-Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
-Output: false
+// Example 3:
+// Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+// Output: false
 
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
@@ -47,7 +47,7 @@ class Solution {
     }
 }
 
-Success
-Details 
-Runtime: 5 ms, faster than 74.11% of Java online submissions for Word Break.
-Memory Usage: 39.4 MB, less than 6.34% of Java online submissions for Word Break.
+// Success
+// Details 
+// Runtime: 5 ms, faster than 74.11% of Java online submissions for Word Break.
+// Memory Usage: 39.4 MB, less than 6.34% of Java online submissions for Word Break.
