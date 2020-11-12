ce252c2f348b59c4829f1f2ead0fdddeab45aa2a
Ruiyue Wang <phynew@hotmail.com>
2020-11-12 17:12:21 -0500

    Update 79. Word Search.java

--- 79. Word Search.java
+++ 79. Word Search.java
@@ -1,27 +1,27 @@
-Medium
+// Medium
 
-Given a 2D board and a word, find if the word exists in the grid.
+// Given a 2D board and a word, find if the word exists in the grid.
 
-The word can be constructed from letters of sequentially adjacent cells, where "adjacent" cells are horizontally or vertically neighboring. 
-The same letter cell may not be used more than once.
+// The word can be constructed from letters of sequentially adjacent cells, where "adjacent" cells are horizontally or vertically neighboring. 
+// The same letter cell may not be used more than once.
 
-Example 1:
-Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
-Output: true
+// Example 1:
+// Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+// Output: true
 
-Example 2:
-Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
-Output: true
+// Example 2:
+// Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+// Output: true
 
-Example 3:
-Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
-Output: false
+// Example 3:
+// Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+// Output: false
  
-Constraints:
-board and word consists only of lowercase and uppercase English letters.
-1 <= board.length <= 200
-1 <= board[i].length <= 200
-1 <= word.length <= 10^3
+// Constraints:
+// board and word consists only of lowercase and uppercase English letters.
+// 1 <= board.length <= 200
+// 1 <= board[i].length <= 200
+// 1 <= word.length <= 10^3
 
 class Solution {
     private char[][] board;
@@ -67,7 +67,7 @@ class Solution {
     }
 }
 
-Success
-Details 
-Runtime: 6 ms, faster than 50.40% of Java online submissions for Word Search.
-Memory Usage: 40.4 MB, less than 10.71% of Java online submissions for Word Search.
+// Success
+// Details 
+// Runtime: 6 ms, faster than 50.40% of Java online submissions for Word Search.
+// Memory Usage: 40.4 MB, less than 10.71% of Java online submissions for Word Search.
