a34c5aa6270e8a7bc01c03050da9e56afc443e1a
Ruiyue Wang <phynew@hotmail.com>
2020-11-12 17:30:36 -0500

    Update 703. Kth Largest Element in a Stream.java

--- 703. Kth Largest Element in a Stream.java
+++ 703. Kth Largest Element in a Stream.java
@@ -1,35 +1,35 @@
-Easy
+// Easy
 
-Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+// Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
 
-Implement KthLargest class:
+// Implement KthLargest class:
 
-KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
-int add(int val) Returns the element representing the kth largest element in the stream.
+// KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+// int add(int val) Returns the element representing the kth largest element in the stream.
  
-Example 1:
-Input
-["KthLargest", "add", "add", "add", "add", "add"]
-[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
-Output
-[null, 4, 5, 5, 8, 8]
+// Example 1:
+// Input
+// ["KthLargest", "add", "add", "add", "add", "add"]
+// [[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+// Output
+// [null, 4, 5, 5, 8, 8]
 
-Explanation
-KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
-kthLargest.add(3);   // return 4
-kthLargest.add(5);   // return 5
-kthLargest.add(10);  // return 5
-kthLargest.add(9);   // return 8
-kthLargest.add(4);   // return 8
+// Explanation
+// KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+// kthLargest.add(3);   // return 4
+// kthLargest.add(5);   // return 5
+// kthLargest.add(10);  // return 5
+// kthLargest.add(9);   // return 8
+// kthLargest.add(4);   // return 8
  
-Constraints:
+// Constraints:
 
-1 <= k <= 104
-0 <= nums.length <= 104
--104 <= nums[i] <= 104
--104 <= val <= 104
-At most 104 calls will be made to add.
-It is guaranteed that there will be at least k elements in the array when you search for the kth element.
+// 1 <= k <= 104
+// 0 <= nums.length <= 104
+// -104 <= nums[i] <= 104
+// -104 <= val <= 104
+// At most 104 calls will be made to add.
+// It is guaranteed that there will be at least k elements in the array when you search for the kth element.
 
 class KthLargest {
     int k;
@@ -58,7 +58,7 @@ class KthLargest {
  * int param_1 = obj.add(val);
  */
  
-Success
-Details 
-Runtime: 1532 ms, faster than 5.02% of Java online submissions for Kth Largest Element in a Stream.
-Memory Usage: 46.8 MB, less than 8.41% of Java online submissions for Kth Largest Element in a Stream.
+// Success
+// Details 
+// Runtime: 1532 ms, faster than 5.02% of Java online submissions for Kth Largest Element in a Stream.
+// Memory Usage: 46.8 MB, less than 8.41% of Java online submissions for Kth Largest Element in a Stream.
