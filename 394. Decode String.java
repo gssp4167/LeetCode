6924b2d41087912a3113bdd14e507b5b959f12fe
Ruiyue Wang <phynew@hotmail.com>
2020-11-20 16:13:30 -0500

    Update 394. Decode String.java

--- 394. Decode String.java
+++ 394. Decode String.java
@@ -1,33 +1,34 @@
-Medium
-Given an encoded string, return its decoded string.
-The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. 
-Note that k is guaranteed to be a positive integer.
-You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
-Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. 
-For example, there won't be input like 3a or 2[4].
+// Medium
 
-Example 1:
-Input: s = "3[a]2[bc]"
-Output: "aaabcbc"
+// Given an encoded string, return its decoded string.
+// The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. 
+// Note that k is guaranteed to be a positive integer.
+// You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
+// Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. 
+// For example, there won't be input like 3a or 2[4].
 
-Example 2:
-Input: s = "3[a2[c]]"
-Output: "accaccacc"
+// Example 1:
+// Input: s = "3[a]2[bc]"
+// Output: "aaabcbc"
 
-Example 3:
-Input: s = "2[abc]3[cd]ef"
-Output: "abcabccdcdcdef"
+// Example 2:
+// Input: s = "3[a2[c]]"
+// Output: "accaccacc"
 
-Example 4:
-Input: s = "abc3[cd]xyz"
-Output: "abccdcdcdxyz"
+// Example 3:
+// Input: s = "2[abc]3[cd]ef"
+// Output: "abcabccdcdcdef"
+
+// Example 4:
+// Input: s = "abc3[cd]xyz"
+// Output: "abccdcdcdxyz"
  
-Constraints:
+// Constraints:
 
-1 <= s.length <= 30
-s consists of lowercase English letters, digits, and square brackets '[]'.
-s is guaranteed to be a valid input.
-All the integers in s are in the range [1, 300].
+// 1 <= s.length <= 30
+// s consists of lowercase English letters, digits, and square brackets '[]'.
+// s is guaranteed to be a valid input.
+// All the integers in s are in the range [1, 300].
 
 Solution #1
 class Solution {
@@ -71,7 +72,7 @@ class Solution {
     }
 }
 
-Success
-Details 
-Runtime: 1 ms, faster than 62.33% of Java online submissions for Decode String.
-Memory Usage: 36.7 MB, less than 97.79% of Java online submissions for Decode String.
+// Success
+// Details 
+// Runtime: 1 ms, faster than 62.33% of Java online submissions for Decode String.
+// Memory Usage: 36.7 MB, less than 97.79% of Java online submissions for Decode String.
