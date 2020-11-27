6e922d7628cb6ad65d8f7c3fa95042be6bd5d7a5
Ruiyue Wang <phynew@hotmail.com>
2020-11-27 00:30:49 -0500

    Update 253. Meeting Rooms II.java

--- 253. Meeting Rooms II.java
+++ 253. Meeting Rooms II.java
@@ -1,17 +1,17 @@
-Medium
+// Medium
 
-Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), 
-find the minimum number of conference rooms required.
+// Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), 
+// find the minimum number of conference rooms required.
 
-Example 1:
-Input: [[0, 30],[5, 10],[15, 20]]
-Output: 2
+// Example 1:
+// Input: [[0, 30],[5, 10],[15, 20]]
+// Output: 2
 
-Example 2:
-Input: [[7,10],[2,4]]
-Output: 1
+// Example 2:
+// Input: [[7,10],[2,4]]
+// Output: 1
 
-NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
+// NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
 
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
@@ -38,7 +38,7 @@ class Solution {
     }
 }
 
-Success
-Details 
-Runtime: 8 ms, faster than 18.46% of Java online submissions for Meeting Rooms II.
-Memory Usage: 38.7 MB, less than 84.21% of Java online submissions for Meeting Rooms II.
+// Success
+// Details 
+// Runtime: 8 ms, faster than 18.46% of Java online submissions for Meeting Rooms II.
+// Memory Usage: 38.7 MB, less than 84.21% of Java online submissions for Meeting Rooms II.
