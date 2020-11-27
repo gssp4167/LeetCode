72ba705bfcb588531d09873ec4a261e05d3e6cb3
Ruiyue Wang <phynew@hotmail.com>
2020-11-27 16:28:27 -0500

    Update 242. Valid Anagram.java

--- 242. Valid Anagram.java
+++ 242. Valid Anagram.java
@@ -1,22 +1,22 @@
-Easy
+// Easy
 
-Given two strings s and t , write a function to determine if t is an anagram of s.
+// Given two strings s and t , write a function to determine if t is an anagram of s.
 
-Example 1:
-Input: s = "anagram", t = "nagaram"
-Output: true
+// Example 1:
+// Input: s = "anagram", t = "nagaram"
+// Output: true
 
-Example 2:
-Input: s = "rat", t = "car"
-Output: false
+// Example 2:
+// Input: s = "rat", t = "car"
+// Output: false
 
-Note:
-You may assume the string contains only lowercase alphabets.
+// Note:
+// You may assume the string contains only lowercase alphabets.
 
-Follow up:
-What if the inputs contain unicode characters? How would you adapt your solution to such case?
+// Follow up:
+// What if the inputs contain unicode characters? How would you adapt your solution to such case?
 
-Solution #1
+// Solution #1
 class Solution {
     public boolean isAnagram(String s, String t) {
         Map<Character, Integer> hp = new HashMap<>();
@@ -46,7 +46,7 @@ class Solution {
     }
 }
 
-Success
-Details 
-Runtime: 16 ms, faster than 10.48% of Java online submissions for Valid Anagram.
-Memory Usage: 43.6 MB, less than 5.12% of Java online submissions for Valid Anagram.
+// Success
+// Details 
+// Runtime: 16 ms, faster than 10.48% of Java online submissions for Valid Anagram.
+// Memory Usage: 43.6 MB, less than 5.12% of Java online submissions for Valid Anagram.
