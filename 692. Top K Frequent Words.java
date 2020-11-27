4481fc04e2a0a9fa50581c0f1471ff417e2662a8
Ruiyue Wang <phynew@hotmail.com>
2020-11-27 15:07:07 -0500

    Update 692. Top K Frequent Words.java

--- 692. Top K Frequent Words.java
+++ 692. Top K Frequent Words.java
@@ -58,3 +58,25 @@ class Solution {
 // Details 
 // Runtime: 5 ms, faster than 85.78% of Java online submissions for Top K Frequent Words.
 // Memory Usage: 39.1 MB, less than 75.68% of Java online submissions for Top K Frequent Words.
+
+// Solution #2
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> word_count = new HashMap<> ();
+        
+        for (String word : words) {
+            word_count.put(word, word_count.getOrDefault(word, 0) + 1);
+        }
+        
+        List<String> candidates = new ArrayList<> (word_count.keySet());
+        
+        Collections.sort(candidates, (w1, w2) -> word_count.get(w1).equals(word_count.get(w2)) ? w1.compareTo(w2) : word_count.get(w2) - word_count.get(w1));
+        
+        return candidates.subList(0, k);
+    }
+}
+
+// Success
+// Details 
+// Runtime: 10 ms, faster than 5.69% of Java online submissions for Top K Frequent Words.
+// Memory Usage: 42.1 MB, less than 5.75% of Java online submissions for Top K Frequent Words.
