adf362b7cf00fd2a2ae65dafc964e3fb7fabe228
Ruiyue Wang <phynew@hotmail.com>
2020-11-27 14:45:29 -0500

    Update 692. Top K Frequent Words.java

--- 692. Top K Frequent Words.java
+++ 692. Top K Frequent Words.java
@@ -1,26 +1,26 @@
-Medium
+// Medium
 
-Given a non-empty list of words, return the k most frequent elements.
+// Given a non-empty list of words, return the k most frequent elements.
 
-Your answer should be sorted by frequency from highest to lowest. 
-If two words have the same frequency, then the word with the lower alphabetical order comes first.
+// Your answer should be sorted by frequency from highest to lowest. 
+// If two words have the same frequency, then the word with the lower alphabetical order comes first.
 
-Example 1:
-Input: ["i", "love", "leetcode", "i", "love", "coding"], k = 2
-Output: ["i", "love"]
-Explanation: "i" and "love" are the two most frequent words.
-Note that "i" comes before "love" due to a lower alphabetical order.
+// Example 1:
+// Input: ["i", "love", "leetcode", "i", "love", "coding"], k = 2
+// Output: ["i", "love"]
+// Explanation: "i" and "love" are the two most frequent words.
+// Note that "i" comes before "love" due to a lower alphabetical order.
 
-Example 2:
-Input: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"], k = 4
-Output: ["the", "is", "sunny", "day"]
-Explanation: "the", "is", "sunny" and "day" are the four most frequent words, with the number of occurrence being 4, 3, 2 and 1 respectively.
-Note:
-You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
-Input words contain only lowercase letters.
+// Example 2:
+// Input: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"], k = 4
+// Output: ["the", "is", "sunny", "day"]
+// Explanation: "the", "is", "sunny" and "day" are the four most frequent words, with the number of occurrence being 4, 3, 2 and 1 respectively.
+// Note:
+// You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+// Input words contain only lowercase letters.
 
-Follow up:
-Try to solve it in O(n log k) time and O(n) extra space.
+// Follow up:
+// Try to solve it in O(n log k) time and O(n) extra space.
 
 Solution #1
 class Solution {
@@ -54,7 +54,7 @@ class Solution {
     }
 }
 
-Success
-Details 
-Runtime: 5 ms, faster than 85.78% of Java online submissions for Top K Frequent Words.
-Memory Usage: 39.1 MB, less than 75.68% of Java online submissions for Top K Frequent Words.
+// Success
+// Details 
+// Runtime: 5 ms, faster than 85.78% of Java online submissions for Top K Frequent Words.
+// Memory Usage: 39.1 MB, less than 75.68% of Java online submissions for Top K Frequent Words.
