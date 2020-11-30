0381ae99a19459537c157f4f5d1cf13f7a3b9dad
Ruiyue Wang <phynew@hotmail.com>
2020-11-29 23:41:40 -0500

    Update 347. Top K Frequent Elements.java

--- 347. Top K Frequent Elements.java
+++ 347. Top K Frequent Elements.java
@@ -1,21 +1,22 @@
-Medium
+// Medium
 
-Given a non-empty array of integers, return the k most frequent elements.
+// Given a non-empty array of integers, return the k most frequent elements.
 
-Example 1:
-Input: nums = [1,1,1,2,2,3], k = 2
-Output: [1,2]
+// Example 1:
+// Input: nums = [1,1,1,2,2,3], k = 2
+// Output: [1,2]
 
-Example 2:
-Input: nums = [1], k = 1
-Output: [1]
+// Example 2:
+// Input: nums = [1], k = 1
+// Output: [1]
 
-Note:
-You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
-Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
-It's guaranteed that the answer is unique, in other words the set of the top k frequent elements is unique.
-You can return the answer in any order.
+// Note:
+// You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+// Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+// It's guaranteed that the answer is unique, in other words the set of the top k frequent elements is unique.
+// You can return the answer in any order.
 
+// Solution #1
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> hm = new HashMap<>();
@@ -47,7 +48,7 @@ class Solution {
     }
 }
 
-Success
-Details 
-Runtime: 11 ms, faster than 39.68% of Java online submissions for Top K Frequent Elements.
-Memory Usage: 41.4 MB, less than 78.11% of Java online submissions for Top K Frequent Elements.
+// Success
+// Details 
+// Runtime: 11 ms, faster than 39.68% of Java online submissions for Top K Frequent Elements.
+// Memory Usage: 41.4 MB, less than 78.11% of Java online submissions for Top K Frequent Elements.
