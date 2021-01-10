0799c9d6305ca1939bd0e00943242e521c9b11a5
Ruiyue Wang <phynew@hotmail.com>
2021-01-10 14:46:59 -0500

    Update 415. Add Strings.java

--- 415. Add Strings.java
+++ 415. Add Strings.java
@@ -1,13 +1,13 @@
-Easy
+// Easy
 
-Given two non-negative integers num1 and num2 represented as string, return the sum of num1 and num2.
+// Given two non-negative integers num1 and num2 represented as string, return the sum of num1 and num2.
 
-Note:
+// Note:
 
-The length of both num1 and num2 is < 5100.
-Both num1 and num2 contains only digits 0-9.
-Both num1 and num2 does not contain any leading zero.
-You must not use any built-in BigInteger library or convert the inputs to integer directly.
+// The length of both num1 and num2 is < 5100.
+// Both num1 and num2 contains only digits 0-9.
+// Both num1 and num2 does not contain any leading zero.
+// You must not use any built-in BigInteger library or convert the inputs to integer directly.
 
 class Solution {
     public String addStrings(String num1, String num2) {
