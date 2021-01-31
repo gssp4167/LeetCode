39176e005d84ee9c90e2b43b936e95be284d5f84
Ruiyue Wang <phynew@hotmail.com>
2021-01-31 14:03:00 -0500

    Update 278. First Bad Version.java

--- 278. First Bad Version.java
+++ 278. First Bad Version.java
@@ -61,3 +61,34 @@ public class Solution extends VersionControl {
 // Details 
 // Runtime: 13 ms, faster than 42.08% of Java online submissions for First Bad Version.
 // Memory Usage: 35.5 MB, less than 90.84% of Java online submissions for First Bad Version.
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        if (n == 1) {
+            return n;
+        }
+        
+        
+        int left = 0;
+        int right = n;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            if (isBadVersion(mid) == true) {
+                right = mid;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+}
+
+// Success
+// Details 
+// Runtime: 15 ms, faster than 30.12% of Java online submissions for First Bad Version.
+// Memory Usage: 35.3 MB, less than 96.00% of Java online submissions for First Bad Version.
+
