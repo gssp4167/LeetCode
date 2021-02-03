41640880e4b77b715aee6e3e4dc0b9b32d09ed15
Ruiyue Wang <phynew@hotmail.com>
2021-02-02 21:04:52 -0500

    Update 350. Intersection of Two Arrays II.java

--- 350. Intersection of Two Arrays II.java
+++ 350. Intersection of Two Arrays II.java
@@ -1,24 +1,24 @@
-Easy
+// Easy
 
-Given two arrays, write a function to compute their intersection.
+// Given two arrays, write a function to compute their intersection.
 
-Example 1:
-Input: nums1 = [1,2,2,1], nums2 = [2,2]
-Output: [2,2]
+// Example 1:
+// Input: nums1 = [1,2,2,1], nums2 = [2,2]
+// Output: [2,2]
 
-Example 2:
-Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
-Output: [4,9]
+// Example 2:
+// Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+// Output: [4,9]
 
-Note:
-Each element in the result should appear as many times as it shows in both arrays.
-The result can be in any order.
+// Note:
+// Each element in the result should appear as many times as it shows in both arrays.
+// The result can be in any order.
 
-Follow up:
-What if the given array is already sorted? How would you optimize your algorithm?
-What if nums1's size is small compared to nums2's size? Which algorithm is better?
-What if elements of nums2 are stored on disk, 
-and the memory is limited such that you cannot load all elements into the memory at once?
+// Follow up:
+// What if the given array is already sorted? How would you optimize your algorithm?
+// What if nums1's size is small compared to nums2's size? Which algorithm is better?
+// What if elements of nums2 are stored on disk, 
+// and the memory is limited such that you cannot load all elements into the memory at once?
 
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
@@ -56,7 +56,7 @@ class Solution {
     }
 }
 
-Success
-Details 
-Runtime: 2 ms, faster than 95.76% of Java online submissions for Intersection of Two Arrays II.
-Memory Usage: 39.7 MB, less than 18.65% of Java online submissions for Intersection of Two Arrays II.
+// Success
+// Details 
+// Runtime: 2 ms, faster than 95.76% of Java online submissions for Intersection of Two Arrays II.
+// Memory Usage: 39.7 MB, less than 18.65% of Java online submissions for Intersection of Two Arrays II.
