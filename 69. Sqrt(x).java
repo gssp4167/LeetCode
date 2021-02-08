83488183baa43968640c1ffef0624b47a7603ddd
Ruiyue Wang <phynew@hotmail.com>
2021-02-07 23:18:02 -0500

    Update 69. Sqrt(x).java

--- 69. Sqrt(x).java
+++ 69. Sqrt(x).java
@@ -1,20 +1,20 @@
-Easy
+// Easy
 
-Given a non-negative integer x, compute and return the square root of x.
+// Given a non-negative integer x, compute and return the square root of x.
 
-Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+// Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
 
-Example 1:
-Input: x = 4
-Output: 2
+// Example 1:
+// Input: x = 4
+// Output: 2
 
-Example 2:
-Input: x = 8
-Output: 2
-Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
+// Example 2:
+// Input: x = 8
+// Output: 2
+// Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
  
-Constraints:
-0 <= x <= 231 - 1
+// Constraints:
+// 0 <= x <= 231 - 1
 
 class Solution {
     public int mySqrt(int x) {
@@ -28,7 +28,7 @@ class Solution {
     }
 }
 
-Success
-Details 
-Runtime: 18 ms, faster than 7.76% of Java online submissions for Sqrt(x).
-Memory Usage: 36 MB, less than 66.92% of Java online submissions for Sqrt(x).
+// Success
+// Details 
+// Runtime: 18 ms, faster than 7.76% of Java online submissions for Sqrt(x).
+// Memory Usage: 36 MB, less than 66.92% of Java online submissions for Sqrt(x).
