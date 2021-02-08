4dbf6f2c846007c43f0a5ab37d777aa18bc540e4
Ruiyue Wang <phynew@hotmail.com>
2021-02-07 22:39:35 -0500

    Create 270. Closest Binary Search Tree Value.java

---
+++ 270. Closest Binary Search Tree Value.java
@@ -0,0 +1,64 @@
+Easy
+
+Given a non-empty binary search tree and a target value, 
+find the value in the BST that is closest to the target.
+
+Note:
+Given target value is a floating point.
+You are guaranteed to have only one unique value in the BST that is closest to the target.
+
+Example:
+Input: root = [4,2,5,1,3], target = 3.714286
+
+    4
+   / \
+  2   5
+ / \
+1   3
+
+Output: 4
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int closestValue(TreeNode root, double target) {
+        int ans = root.val;
+        double diff = Math.abs(target - root.val);
+        
+        while (root != null) {            
+            if (Math.abs(target - root.val) < diff) {
+                diff = Math.abs(target - root.val);
+                ans = root.val;
+            } 
+            
+            if (diff == 0) {
+                return ans;
+            }
+            else if (target < root.val) {
+                root = root.left;
+            }
+            else {          
+                root = root.right;
+            }                      
+        }
+        
+        return ans;
+    }
+}
+
+Details 
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Closest Binary Search Tree Value.
+Memory Usage: 38.7 MB, less than 76.45% of Java online submissions for Closest Binary Search Tree Value.
