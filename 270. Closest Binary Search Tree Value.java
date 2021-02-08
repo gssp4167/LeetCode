fb514b68970240880373785da075686cd2282079
Ruiyue Wang <phynew@hotmail.com>
2021-02-07 23:01:02 -0500

    Update 270. Closest Binary Search Tree Value.java

--- 270. Closest Binary Search Tree Value.java
+++ 270. Closest Binary Search Tree Value.java
@@ -62,3 +62,37 @@ class Solution {
 // Details 
 // Runtime: 0 ms, faster than 100.00% of Java online submissions for Closest Binary Search Tree Value.
 // Memory Usage: 38.7 MB, less than 76.45% of Java online submissions for Closest Binary Search Tree Value.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int closestValue(TreeNode root, double target) {
+        int val, closest = root.val;
+        
+        while (root != null) {
+            val = root.val;
+            closest = (Math.abs(val - target) < Math.abs(closest - target)) ? val : closest;
+            root = (target < val) ? root.left : root.right;
+        }
+        
+        return closest;
+    }
+}
+
+// Success
+// Details 
+// Runtime: 0 ms, faster than 100.00% of Java online submissions for Closest Binary Search Tree Value.
+// Memory Usage: 38.8 MB, less than 64.39% of Java online submissions for Closest Binary Search Tree Value.
